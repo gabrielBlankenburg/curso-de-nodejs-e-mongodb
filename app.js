b7cd9f5bf630ad741b791677ae83fbb8b50068ed
gabrielBlankenburg <gabriel.blankenburg.p.silva@gmail.com>
2017-08-28 19:47:01 -0300

    arquivos ate a aula 18

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 // chama o express, e ele retorna uma função
 var express = require('express');
+// chama o modulo teste
+var msg = require('./mod_test');
 // executa a funcao que o express retornou
 var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// Configura resposta para o /tecnologia
-app.get('/tecnologia', function(req, res){
-	res.render('secao/tecnologia');
-});
-
 // Configura resposta para o root ('/')
 app.get('/', function(req, res){
-	res.send('<html><body>Portal de Noticias</body></html>');
+	res.render('home/index');
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+	res.render('admin/form_add_noticia');
+});
+
+app.get('/noticias', function(req, res){
+	res.render('noticias/noticias');
 });
 
 // Escuta requisições na porta 3000
 app.listen(3000, function(){
-	console.log('servidor funcionando');
+	console.log(msg());
 })
---
+++ mod_test.js
@@ -0,0 +1,7 @@
+
+
+// mostra o que exportar, no caso a variavel msg
+module.exports = function(){
+	var msg = 'Este módulo contém apenas uma string';
+	return msg;
+}
