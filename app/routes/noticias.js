a0293a406ec14fe733d8328d4c8a42865353285a
gabrielBlankenburg <gabriel.blankenburg.p.silva@gmail.com>
2017-08-28 21:12:26 -0300

    Arquivos ate a aula 24

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,23 @@
 // Recebe o parametro app (que vem do express)
 module.exports = function(app){
 	app.get('/noticias', function(req, res){
-		res.render('noticias/noticias');
+
+		// modulo para poder usar o mysql
+		var mysql = require('mysql');
+
+		// Efetua a conexão
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '1234',
+			database: 'portal_noticias'
+		});
+
+		// parametros sao a query e o callback
+		connection.query('select * from noticias', function(error, result){
+			res.send(result);
+		});
+
 	});
 };
 	
