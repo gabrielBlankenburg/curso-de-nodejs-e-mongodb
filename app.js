bbd4894fb1f0954ccf9b8dc7f82baaca223aa847
gabrielBlankenburg <gabriel.blankenburg.p.silva@gmail.com>
2017-09-05 17:34:41 -0300

    arquivos até aula 30

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var app = require('./config/server');
 
 // Chama a rota de noticias
-var rotaNoticias = require('./app/routes/noticias')(app);
+// var rotaNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home')(app);
+// var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+// var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 // Escuta requisições na porta 3000
 app.listen(3000, function(){
--- config/server.js
+++ config/server.js
@@ -1,5 +1,7 @@
 // chama o express, e ele retorna uma função
 var express = require('express');
+// chama o consign que é responsável por diminuir o número de linhas quando tem várias páginas
+var consign = require('consign');
 // executa a funcao que o express retornou, passando o app como parametro
 var app = express(app);
 // configura diretorio de views padrão
@@ -7,5 +9,8 @@ app.set('views', './app/views')
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// o consign reconhece todos os modules de routes e manda para o app
+consign().include('app/routes').into(app)
 
 module.exports = app;
